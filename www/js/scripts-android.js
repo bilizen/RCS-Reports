ca826c4fca13b6e949beee65e9915aca779b53ac
Henry <haruhi_01@hotmail.com>
2016-05-11 14:50:49 -0500

    check box ios impuesto

--- www/js/scripts-android.js
+++ www/js/scripts-android.js
@@ -1,11 +1,5 @@
 
-  $(function () {
-    $.material.init();
-  });
+$(function () {
+	$.material.init();
+});
 
-   $('.button-collapse').sideNav({
-      menuWidth: 300, // Default is 240
-      edge: 'right', // Choose the horizontal origin
-      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    }
-  );
